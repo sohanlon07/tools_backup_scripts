636e6310322a79664fa0c7170cee1a007d7cdc47
Shane OHanlon <sohanlon07@gmail.com>
2017-02-06 22:04:33 +0000

    added variables

--- backuptools.sh
+++ backuptools.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-rsync -av --delete ~/Library/Application\ Support/Sublime\ Text\ 3 ~/Google\ Drive/Documents/Profiles_Backup
-rsync -av --delete ~/.npm* ~/Google\ Drive/Documents/Profiles_Backup/npm_backup
-rsync -av --delete ~/.bash* ~/Google\ Drive/Documents/Profiles_Backup/bash_backup
-rsync -av --delete ~/.docker* ~/Google\ Drive/Documents/Profiles_Backup/docker_backup
-rsync -av --delete ~/.bosh* ~/Google\ Drive/Documents/Profiles_Backup/bosh_backup
-rsync -av --delete ~/.gem ~/Google\ Drive/Documents/Profiles_Backup/gem_backup
-rsync -av --delete ~/.git* ~/Google\ Drive/Documents/Profiles_Backup/git_backup
-rsync -av --delete ~/.groovy ~/Google\ Drive/Documents/Profiles_Backup/groovy_backup
-rsync -av --delete ~/.rvm ~/Google\ Drive/Documents/Profiles_Backup/rvm_backup
-rsync -av --delete --exclude=workspace ~/.jenkins ~/Google\ Drive/Documents/Profiles_Backup/jenkins_backup
-rsync -av --delete ~/.m2 ~/Google\ Drive/Documents/Profiles_Backup/m2_backup
-rsync -av --delete ~/.ssh ~/Google\ Drive/Documents/Profiles_Backup/ssh_backup
-rsync -av --delete ~/.viminfo ~/Google\ Drive/Documents/Profiles_Backup/viminfo_backup
+
+#Google Drive on mac defaults to "Google Drive" on Install.
+BACKUPPTH=~/Google\ Drive/Profiles_Backup
+
+#If exists, will ignore creation of folder
+#$BACKUPPTH inside quotes to cater for space in "Google Drive"
+mkdir "$BACKUPPTH"
+
+#rsync will create backup folders
+#For jenkins, omitting workspace folder
+rsync -av --delete ~/Library/Application\ Support/Sublime\ Text\ 3 ""$BACKUPPTH""/sublime_backup
+rsync -av --delete ~/.npm* "$BACKUPPTH"/npm_backup
+rsync -av --delete ~/.bash* "$BACKUPPTH"/bash_backup
+rsync -av --delete ~/.docker* "$BACKUPPTH"/docker_backup
+rsync -av --delete ~/.bosh* "$BACKUPPTH"/bosh_backup
+rsync -av --delete ~/.gem "$BACKUPPTH"/gem_backup
+rsync -av --delete ~/.git* "$BACKUPPTH"/git_backup
+rsync -av --delete ~/.groovy "$BACKUPPTH"/groovy_backup
+rsync -av --delete ~/.rvm "$BACKUPPTH"/rvm_backup
+rsync -av --delete --exclude=workspace ~/.jenkins "$BACKUPPTH"/jenkins_backup
+rsync -av --delete ~/.m2 "$BACKUPPTH"/m2_backup
+rsync -av --delete ~/.ssh "$BACKUPPTH"/ssh_backup
+rsync -av --delete ~/.viminfo "$BACKUPPTH"/viminfo_backup
+
+echo "Sync Completed"
