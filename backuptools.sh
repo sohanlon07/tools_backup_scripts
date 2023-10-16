febe5ac3d456f8896a04fc84e7fe6d326f7f16b3
Shane O'Hanlon <sohanlon@Shanes-MacBook-Air.local>
2023-10-16 17:37:31 +0100

    minor updaes for zsh

--- backuptools.sh
+++ backuptools.sh
@@ -9,14 +9,17 @@ mkdir "$BACKUPPTH"
 
 #rsync will create backup folders
 #For jenkins, omitting workspace folder
-rsync -av --delete ~/Library/Application\ Support/Sublime\ Text\ 3 "$BACKUPPTH"/sublime_backup
 rsync -av --delete ~/.npm* "$BACKUPPTH"/npm_backup
+rsync -av --delete ~/.nvm* "$BACKUPPTH"/nvm_backup
 rsync -av --delete ~/.bash* "$BACKUPPTH"/bash_backup
+rsync -av --delete ~/.zsh* "$BACKUPPTH"/zshbackup
 rsync -av --delete ~/.docker* "$BACKUPPTH"/docker_backup
 rsync -av --delete ~/.bosh* "$BACKUPPTH"/bosh_backup
 rsync -av --delete ~/.gem "$BACKUPPTH"/gem_backup
 rsync -av --delete ~/.git* "$BACKUPPTH"/git_backup
 rsync -av --delete ~/.groovy "$BACKUPPTH"/groovy_backup
+rsync -av --delete ~/.python* "$BACKUPPTH"/python_backup
+rsync -av --delete ~/.pyenv* "$BACKUPPTH"/pyenv_backup
 rsync -av --delete ~/.rvm "$BACKUPPTH"/rvm_backup
 rsync -av --delete --exclude=workspace ~/.jenkins "$BACKUPPTH"/jenkins_backup
 rsync -av --delete ~/.m2 "$BACKUPPTH"/m2_backup
@@ -25,9 +28,9 @@ rsync -av --delete ~/.viminfo "$BACKUPPTH"/viminfo
 rsync -av --delete ~/.iterm2 "$BACKUPPTH"/iterm2
 rsync -av --delete ~/.iterm2 "$BACKUPPTH"/iterm2/iterm2_shell_integration.bash
 
-echo "Running mySql Backup"
-brew services start mysql
-mysqldump -uroot -p -v --all-databases > "$BACKUPPTH"/all_databases.sql
+# echo "Running mySql Backup"
+# brew services start mysql
+# mysqldump -uroot -p -v --all-databases > "$BACKUPPTH"/all_databases.sql
 
 echo "Creating HomeBrew Restore Script"
 sh backup-brew.sh -v > "$BACKUPPTH"/restore-homebrew.sh && chmod +x "$BACKUPPTH"/restore-homebrew.sh
